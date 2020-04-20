626dc8eb6764bdc13088d8aa9547d7702936f576
Israel <Israel.pro1994@gmail.com>
2020-04-20 14:30:31 -0500

    created components

---
+++ src/Calculator.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default class Calculator extends React.Component{
+
+    render(){
+        return(
+            <div>
+                here we got work
+            </div>
+        )
+    }
+
+}
